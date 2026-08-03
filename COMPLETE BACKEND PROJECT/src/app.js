0125e59dee23d610b8eb06a4015f6fc02b9da58a
Arun Kumar <arunkumar239664@gmail.com>
2026-08-03 17:27:12 +0530

    Add backend project

--- COMPLETE BACKEND PROJECT/src/app.js
+++ COMPLETE BACKEND PROJECT/src/app.js
@@ -8,16 +8,25 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 // cors configurations
-(app.use(
+app.use(
   cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
-),
+);
+
+// import routes
+
+import healthCheckRouter from "./routes/healthckeck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
+
+
+
   app.get("/", (req, res) => {
     res.send("Welcome to basecampy");
-  }));
+  });
 
 export default app;
