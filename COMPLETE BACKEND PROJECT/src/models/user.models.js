0c514356cf5f815ccb2c56e5dba21cb67f16c268
Arun Kumar <arunkumar239664@gmail.com>
2026-08-12 19:11:02 +0530

    user schema added

---
+++ COMPLETE BACKEND PROJECT/src/models/user.models.js
@@ -0,0 +1,63 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placeholder.co/200x200`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true, // check if same username exist
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"], // add custom error
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
